56a432547248921223f6054e3250b1e9ae6e9766
Hibiscus4431 <159493880+Hibiscus4431@users.noreply.github.com>
2024-02-20 18:31:46 -0600

    Create #some bullshit.py

---
+++ #some bullshit.py
@@ -0,0 +1,8 @@
+#some bullshit
+#main function
+#what to do:
+# TODO initialize variables and libraries
+# TODO call movement algorithms and update at .5 increments NE-1 implementation
+# TODO output movement to .txt file for plot check
+# variables to initialize: pos, vel, dir, acc, lin acc, orient, timestep, subvariables for maxes
+print("Hello world")
