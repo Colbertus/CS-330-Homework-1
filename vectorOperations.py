ea577de6411cb8a2aabd566971aa00010e4ecf0d
Colby McClure <66743883+Colbertus@users.noreply.github.com>
2024-02-21 00:26:29 +0000

    Vector Class Implemented

---
+++ vectorOperations.py
@@ -0,0 +1,31 @@
+from numpy import pi, sqrt 
+
+class vector:
+
+    def init(self, x, z):
+        self.x = x
+        self.z = z 
+
+    def add(self, other):
+        sumVect = vector(self.x + other.x, self.z + other.z)
+        return sumVect 
+
+    def sub(self, other):
+        subVect = vector(self.x - other.x, self.z - other.z)
+        return subVect 
+
+    def mul(self, other):
+        mulVect = vector(self.x * other, self.z * other)
+        return mulVect 
+
+    def length(self): 
+        return sqrt((self.x * self.x) + (self.z * self.z))
+
+    def normalize(self):
+        distance = self.length()
+        returnVect = vector(self.x / distance, self.z / distance)
+        return returnVect 
+
+    def print(self):
+        print("X: " + str(self.x) + "\nZ: " + str(self.z)) 
+
